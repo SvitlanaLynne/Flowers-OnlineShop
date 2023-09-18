3fcb27b004fde3776d586c3d5d3ee70c2768e274
Svitlana <Svetlanalynne@gmail.com>
2023-09-18 11:15:42 -0300

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react-hooks/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>Flowers Shop</div>
+  );
+}
+
+export default App;
